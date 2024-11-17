fb1b16fbf4fafe69ebf1862b1d5dfea76b896835
Matheus Moreira <mmoreira@fastmail.com>
2024-11-17 13:16:44 +0100

    Add pre-commit hook to run cljfmt

--- test/zero_to_hero/core_test.clj
+++ test/zero_to_hero/core_test.clj
@@ -1,7 +1,7 @@
 (ns zero-to-hero.core-test
   #_(:require [clojure.test :refer :all]
-            [zero-to-hero.core :refer :all]))
+              [zero-to-hero.core :refer :all]))
 
 #_(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+    (testing "FIXME, I fail."
+      (is (= 1 1))))
